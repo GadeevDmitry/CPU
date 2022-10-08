3a66f3a786c8a2bf6ea28e40e00954e4128ae64e
GadeevDmitry <gadeev.dima@mail.ru>
2022-10-08 15:25:01 +0300

    add check_signature()

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -10,9 +10,36 @@ struct cpu_store
     size_t execution_size;
 };
 
+/*-----------------------------------------FUNCTION_DECLARATION-----------------------------------------*/
+
+bool check_signature(cpu_store *progress);
+
+/*------------------------------------------------------------------------------------------------------*/
+
 int main(int argc, char *argv[])
 {
-    struct cpu_store progress = {};
+    cpu_store progress = {};
     progress.execution.machine_code = read_file(argv[1], &progress.execution_size);
+
+    if (!check_signature(&progress)) return 1;
 }
 
+bool check_signature(cpu_store *progress)
+{
+    header signature = *(header *) progress->execution.machine_code;
+
+    if (signature.fst_let != 'G' || signature.sec_let != 'D')
+    {
+        fprintf(stderr, "./CPU: Access denied\n");
+        return false;
+    }
+    if (signature.version != 1)
+    {
+        fprintf(stderr, "./CPU doesn't support the version %d\n", signature.version);
+        return false;
+    }
+
+    progress->execution.machine_pos = sizeof(header);
+    
+    return true;
+}
